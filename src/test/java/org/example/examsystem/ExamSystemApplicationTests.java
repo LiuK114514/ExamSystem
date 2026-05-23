1e8808011230111e0e3af206bca340195ce5fa35
LiuK114514 <302023315185@zjut.edu.cn>
2026-05-23 14:05:53 +0800

    去除spring容器测试

--- src/test/java/org/example/examsystem/ExamSystemApplicationTests.java
+++ src/test/java/org/example/examsystem/ExamSystemApplicationTests.java
@@ -1,11 +1,15 @@
 package org.example.examsystem;
 
+import org.example.examsystem.config.EmbeddedRedisConfig;
+import org.example.examsystem.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import({TestConfig.class, EmbeddedRedisConfig.class})
 class ExamSystemApplicationTests {
 
     @Test
